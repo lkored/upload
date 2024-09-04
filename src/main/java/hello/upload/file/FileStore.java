61bafa7cbe4ad6008759bfb598a701c98a65946b
lkoblue <gangwon20@gmail.com>
2024-09-04 14:01:26 +0900

    최초커밋

---
+++ src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,56 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String fileName) {
+        return fileDir + fileName;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> files) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile file : files) {
+            if(!file.isEmpty()) {
+                storeFileResult.add(storeFile(file));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile file) throws IOException {
+        if(file.isEmpty()) {
+            return null;
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        file.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        String uuid = UUID.randomUUID().toString();
+        String ext = extractExt(originalFilename);
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+}
